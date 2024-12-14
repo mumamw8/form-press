c09a70854de6ac3a8bb47f76610b30f363b8507c
mumamw8 <91581448+mumamw8@users.noreply.github.com>
2024-12-14 17:21:32 -0600

    downgrade eslint

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
     }
   })`
 
-  const integrations = ['Google Sheets!', 'Notion!', 'Airtable!', ];
+  const integrations = ["Google Sheets!", "Notion!", "Airtable!"]
 
   return (
     <>
@@ -35,11 +35,13 @@ export default function Page() {
               <Heading>
                 <span>Create Forms, Deliver Responses,</span>
                 <br />
-                <span className="relative bg-clip-text">
-                Directly to
-                </span>
+                <span className="relative bg-clip-text">Directly to</span>
                 {/* <br /> */}
-                <FlipWords duration={2000} words={integrations} className="text-blue-500" />
+                <FlipWords
+                  duration={2000}
+                  words={integrations}
+                  className="text-blue-500"
+                />
               </Heading>
             </div>
 
@@ -98,7 +100,7 @@ export default function Page() {
                   </p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
                     Get notified about critical events the moment they happen,
-                    no matter if you're at home or on the go.
+                    no matter if you&apos;re at home or on the go.
                   </p>
                 </div>
 
@@ -248,9 +250,12 @@ export default function Page() {
               </div>
 
               <p className="text-base sm:text-lg lg:text-lg/8 font-medium tracking-tight text-brand-950 text-center lg:text-left text-pretty">
+                {`
                 PingPanda has been a game-changer for me. I've been using it for
                 two months now and seeing sales pop up in real-time is super
                 satisfying.
+
+`}
               </p>
 
               <div className="flex flex-col justify-center lg:justify-start sm:flex-row items-center sm:items-start gap-4 mt-2">
@@ -282,9 +287,10 @@ export default function Page() {
               </div>
 
               <p className="text-base sm:text-lg lg:text-lg/8 font-medium tracking-tight text-brand-950 text-center lg:text-left text-pretty">
-                PingPanda's been paying off for our SaaS. Nice to have simple
+                {`                PingPanda's been paying off for our SaaS. Nice to have simple
                 way to see how we're doing day-to-day. Definitely makes our
                 lives easier.
+`}
               </p>
 
               <div className="flex flex-col justify-center lg:justify-start sm:flex-row items-center sm:items-start gap-4 mt-2">

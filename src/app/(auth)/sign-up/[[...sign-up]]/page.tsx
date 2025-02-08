031fc38d2b64107bb32ed39b601018ffd2ce762b
mumamw8 <91581448+mumamw8@users.noreply.github.com>
2025-02-08 14:46:02 -0600

    refactor: simplify SignUp component redirect URLs to root path

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -5,10 +5,7 @@ import { SignUp } from "@clerk/nextjs"
 export default function Page() {
   return (
     <div className="w-full flex-1 flex items-center justify-center">
-      <SignUp
-        fallbackRedirectUrl={"/dashboard"}
-        forceRedirectUrl={"/dashboard"}
-      />
+      <SignUp fallbackRedirectUrl={"/"} forceRedirectUrl={"/"} />
     </div>
   )
 }
